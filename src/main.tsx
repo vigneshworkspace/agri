0336bfbdc12301d3a759738b03e03db60c0fb9dc
K.vigneshwaran <edu.workspace01@gmail.com>
2025-09-07 20:34:19 +0530

    fix: import path for App component

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
-import App from './App';
+import App from '../App';
 import { ClerkProvider } from '@clerk/clerk-react';
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
